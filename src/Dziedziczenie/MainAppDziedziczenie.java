f1f141e816f9af02bff46591b479986f2133a76c
jerzokb <beata@palkabm.net>
2021-03-12 21:39:09 +0100

    Dziedziczenie - setting PC power supply

--- src/Dziedziczenie/MainAppDziedziczenie.java
+++ src/Dziedziczenie/MainAppDziedziczenie.java
@@ -14,16 +14,25 @@ public class MainAppDziedziczenie {
         PC officeComputer = new PC("Office computer", "HP", 500, 128);
         Laptop gamingLaptop = new Laptop("XGames", "HP GAMES", 500, 256, 80);
 
+        // Utworzony officeComputer isPowerSupplyOn = false
+        officeComputer.switchOn();
+        System.out.println(officeComputer.getState());
+
+        // Ustawiam isPowerSupplyOn = true
+        officeComputer.setPowerSupply(true);
         officeComputer.switchOn();
         System.out.println(officeComputer.getState());
 
         officeComputer.showComputerName();
 
+        // Battery level = 80
         gamingLaptop.switchOn(); // najpierw sprawdza w klasie dziecko, potem w klasie matce
         System.out.println(gamingLaptop.getState());
 
+        // Wyłączam laptop
         gamingLaptop.switchOff();
 
+        // Rozładowuję baterię i próbuję włączyć
         gamingLaptop.setBatteryLevel(0);
         gamingLaptop.switchOn(); // najpierw sprawdza w klasie dziecko, potem w klasie matce
         System.out.println(gamingLaptop.getState());
--- src/Dziedziczenie/computer/PC.java
+++ src/Dziedziczenie/computer/PC.java
@@ -2,21 +2,33 @@ package Dziedziczenie.computer;
 
 public class PC extends Computer{
 
+    private boolean isPowerSupplyOn;
+
     // Klasa PC jest komputerem
     // PC ma dostęp do wszystkich publicznych pól i metod klasy matki
 
     public PC(String name, String type, int hdd, int ram) {
         super(name, type, hdd, ram); // wywołanie konstruktora w klasie matce
+        this.isPowerSupplyOn = false;
+    }
+
+    public void setPowerSupply(boolean powerSupplyOn) {
+        this.isPowerSupplyOn = powerSupplyOn;
     }
 
     // Nadpisujemy metodę z klasy matki
     @Override
     public void switchOn() {
         System.out.println("Checking power supply");
-        super.switchOn(); // wywołanie metody z klasy matki
+        if(isPowerSupplyOn) {
+            super.switchOn(); // wywołanie metody z klasy matki
+        } else {
+            System.out.println("Please plug in power supply!");
+        }
     }
 
     public void showComputerName() {
         System.out.println(name);
     }
+
 }
